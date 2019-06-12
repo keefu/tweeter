b944d7d001fdb160c3a5c5f3fb151706747c4b5c
keefu <cyuplia@gmail.com>
2019-06-12 17:11:36 +0000

    Show counter with condition

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
-    $("textarea").on("input", () => {
-    var count = $("textarea").val().length;
-    
+    $("textarea").on("input", (event) => {
+    var count = 140 - $(event.target).val().length;
+    var compteur = $(event.target).parent().find(".counter");
+    compteur.text(count);
+    if(count < 0){
+        compteur.css("color", "red");
+    } else {
+        compteur.css("color", "initial")
+    }
     }); 
-//document.addEventListener("input", () => {
-//    var count = $("textarea").val().length;
-//    console.log(count);  
-//    });
 });
   
